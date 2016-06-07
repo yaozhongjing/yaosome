ecddb7f2a93afcd8ba41945a174bc752d3db3e6d
zhongjing yao <yao@liquidtalent.com>
2016-06-07 17:42:20 -0400

    delopment changes

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ var app = express();
 var root = __dirname + '/public';
 app.use(express.static(root));
 app.use(fallback('index.html', { root: root }));
-app.listen(3000);
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
